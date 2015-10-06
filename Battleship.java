c478589a3fe5eec7c4896338bdd889644c5d10e6
Gregory McQuillan <gmcquillan@gmail.com>
2015-10-05 22:10:40 -0700

    now allowing users to input ship locations

--- Battleship.java
+++ Battleship.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class Battleship {
     private static int MAX_SHIPS = 5;
@@ -6,10 +6,17 @@ public class Battleship {
     public static void main(String[] args) {
         BattleField playerField = new BattleField(10);
 
-        playerField.addShip(1, 1);
-        playerField.addShip(0, 3);
-        playerField.addShip(9, 3);
-        playerField.addShip(2, 1);
+        System.out.println("Get ready to place your ships.");
+        //@TODO: replace the number 5 with the constant MAX_SHIPS
+        System.out.println("You get 5 ships, place them wisely");
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter coordinates for Ship 1: ");
+        String coordinates = keyboard.nextLine();
+
+        Point shipLocation = getPoint(coordinates);
+        playerField.addShip(shipLocation.getX(),
+                shipLocation.getY());
 
         System.out.println("Player's Field:");
         FieldDisplay playerDisplay = new FieldDisplay(playerField, true);
@@ -23,7 +30,27 @@ public class Battleship {
         boolean showComputerShips = (args.length > 0 && args[0].equals("--show-computer-ships"));
         FieldDisplay computerDisplay = new FieldDisplay(computerField, showComputerShips);
         computerDisplay.render();
+    }
 
+    /**
+     * Converts coordinates from A1 to (0, 0)
+     * TODO: handle coordinate values >= 10
+     */
+    private static Point getPoint(String coordinate) {
+        coordinate = coordinate.toUpperCase();
+
+        if (coordinate.length() == 2) {
+            char letter = coordinate.charAt(0);
+            char number = coordinate.charAt(1);
+
+            int x = (letter - 'A');
+            int y = Character.getNumericValue(number) - 1;
+
+            return new Point(x, y);
+        }
+        else {
+            return Point.INVALID_POINT;
+        }
     }
 
 }
